15ec437b29f43a2addda7891f1fef8ef70508574
acalvom <andreac.3108@gmail.com>
2021-04-20 09:57:43 +0200

    install crypto-js & decrypt login password #5

--- app/routes/users/userRest.js
+++ app/routes/users/userRest.js
@@ -1,7 +1,10 @@
+const CryptoJS = require("crypto-js");
+
 function userLogin(req, res) {
     let userEmail = req.body.email;
-    let password = req.body.password;
-    console.log('Email: ' + userEmail + ' Password: ' + password);
+    let passwordEncrypted = req.body.password;
+    console.log('Email: ' + userEmail + ' Password: ' + passwordEncrypted);
+    let password = CryptoJS.AES.decrypt(passwordEncrypted, 'password').toString(CryptoJS.enc.Utf8);
 }
 
 exports.userLogin = userLogin;
