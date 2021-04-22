5eaaba17adbe595c4b3efee2754489420837c15a
acalvom <andreac.3108@gmail.com>
2021-04-22 13:16:55 +0200

    fix sonar issues #5

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const authController = require('../controllers/authController');
 
 const router = Router();
-
+router.disable("x-powered-by");
 router.post('/users/login', authController.login);
 
 // This is only to validate token verification
