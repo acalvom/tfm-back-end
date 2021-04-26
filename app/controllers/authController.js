a7c9795f25be673af1f0641edf27a51bd9e1f99e
acalvom <andreac.3108@gmail.com>
2021-04-26 11:47:42 +0200

    implement "Register" query to create new user & encrypt password in DB #6

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -6,17 +6,26 @@ const httpCode = require('../resources/httpCodes');
 const User = require('../models/User');
 
 var token, sql, email;
-var user;
+var newUser;
 const saltRounds = 10;
 
 function register(req, res) {
-    // TODO
-    user = req.body;
+    let user = req.body;
     if (user) {
-        let newUser = createUser(user)
+        newUser = createUser(user)
         console.log(newUser);
         const comparison = bcrypt.compareSync('a', newUser.password)
         console.log(comparison);
+        sql = 'INSERT INTO users (name, surname, dni, gender, email, password, penalties, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        connection.query(sql, [newUser.name, newUser.surname, newUser.dni, newUser.gender, newUser.email, newUser.password, newUser.penalties, newUser.role], function (err, result) {
+            if (!err) {
+                console.log('Insert successful' + result)
+                res.json('USER INSERTED SUCCESSFULLY');
+            } else {
+                console.log('Insert error' + err)
+                res.status(httpCode.codes.CONFLICT).json('USER ALREADY EXISTS');
+            }
+        })
     }
 }
 
@@ -31,28 +40,38 @@ function createUser(user) {
     newUser.role(user.role);
     newUser.penalties(user.penalties);
     newUser.email(user.email);
-    let encryptedPassword = bcrypt.hashSync(decryptPassword(user.password), saltRounds);
+    let encryptedPassword = bcrypt.hashSync(decryptBodyPassword(user.password), saltRounds);
     newUser.password(encryptedPassword);
     return newUser.build();
 }
 
-function decryptPassword(passwordEncrypted) {
+function decryptBodyPassword(passwordEncrypted) {
     return CryptoJS.AES.decrypt(passwordEncrypted, 'password').toString(CryptoJS.enc.Utf8);
 }
 
 function login(req, res) {
     email = req.body.email;
-    let passwordEncrypted = req.body.password;
-    if (email && passwordEncrypted) {
-        let password = decryptPassword(passwordEncrypted);
-        sql = 'SELECT * FROM users WHERE email = ? and password = ?';
-        connection.query(sql, [email, password], function (err, result) {
+    let password = decryptBodyPassword(req.body.password);
+    if (email && password) {
+        // superuser: $2b$10$AVmvFV1pFySG7toYpJ6Lau2vXabZsxPBdWjkmVvZ/kJ8735zCGl72
+        // teacher: $2b$10$GhW2v/v3xXiPZ9T4gs4wq.FOZ7KmB39wCibjc4E.Tn255qeiIkCx.
+        // student: $2b$10$AdqKmH8DM4/rFoAc5suQJeaziM1gEZzCm6v8g8rVSbK9oVanhimaq
+        // sql = 'SELECT * FROM users WHERE email = ? and password = ?';
+        sql = 'SELECT * FROM users WHERE email = ?';
+        // connection.query(sql, [email, password], function (err, result) {
+        connection.query(sql, [email], function (err, result) {
             if (!err && result.length == 1) {
-                let role = result[0].role;
-                token = middleware.generateToken(email, role);
-                saveToken(role, res);
+                let isCorrectPassword = bcrypt.compareSync(password, result[0].password);
+                if (isCorrectPassword) {
+                    let role = result[0].role;
+                    token = middleware.generateToken(email, role);
+                    saveToken(role, res);
+                } else {
+                    console.log('WRONG PASSWORD');
+                    res.status(httpCode.codes.BADREQUEST).json('WRONG PASSWORD');
+                }
             } else {
-                res.status(httpCode.codes.NOTFOUND).json('NOT FOUND');
+                res.status(httpCode.codes.NOTFOUND).json('USER NOT FOUND');
             }
         });
 
