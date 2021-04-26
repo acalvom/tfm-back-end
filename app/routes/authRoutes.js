4143194ad20e203b8660463b64fa8acaecf94805
acalvom <andreac.3108@gmail.com>
2021-04-26 17:59:15 +0200

    fix sonar #6

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const {Router} = require('express');
 const authController = require('../controllers/authController');
+let helmet = require("helmet");
 
 const router = Router();
+router.use(helmet.hidePoweredBy());
 router.use(function (req, res, next) {
     res.removeHeader("X-Powered-By");
     next();
