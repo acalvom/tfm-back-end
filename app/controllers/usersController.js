59a39f6de07e351a1f91bde6727143920e86809d
acalvom <andreac.3108@gmail.com>
2021-04-29 12:34:41 +0200

    unify getStudents & getTeachers to getAllUsers #7

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,30 +1,15 @@
 const connection = require('../database/database');
 const httpCode = require('../resources/httpCodes');
 
-const User = require('../models/User');
-
-function getStudents(req, res) {
-    sql = 'SELECT * FROM users WHERE role = ?';
-    connection.query(sql, ['student'], function (err, students) {
-        if (!err && students.length > 0) {
-            res.status(httpCode.codes.OK).json(students);
+function getAllUsers(req, res) {
+    sql = 'SELECT * FROM users';
+    connection.query(sql, function (err, users) {
+        if (!err && users.length > 0) {
+            res.status(httpCode.codes.OK).json(users);
         } else
             res.status(httpCode.codes.NOTFOUND).json('NOT FOUND');
     });
 }
 
-exports.getStudents = getStudents;
-
-
-function getTeachers(req, res) {
-    sql = 'SELECT * FROM users WHERE role = ?';
-    connection.query(sql, ['teacher'], function (err, teachers) {
-        if (!err && teachers.length > 0) {
-            res.status(httpCode.codes.OK).json(teachers);
-        } else
-            res.status(httpCode.codes.NOTFOUND).json('NOT FOUND');
-    });
-}
-
-exports.getTeachers = getTeachers;
+exports.getAllUsers = getAllUsers;
 
--- app/routes/usersRoutes.js
+++ app/routes/usersRoutes.js
@@ -6,7 +6,6 @@ const middleware = require('../middleware/middleware');
 const router = express.Router();
 router.use(helmet.hidePoweredBy());
 
-router.get('/users/students', middleware.isAdmin, usersController.getStudents);
-router.get('/users/teachers', middleware.isAdmin, usersController.getTeachers);
+router.get('/users', middleware.isAdmin, usersController.getAllUsers);
 
 module.exports = router;
