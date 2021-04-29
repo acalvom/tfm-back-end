e45f2e0c48100a4cf69dceb8b48c28114f958168
acalvom <andreac.3108@gmail.com>
2021-04-29 12:51:51 +0200

    implement "getAllUsers" tests & coverage report #7

---
+++ test/controllers/testUsersController.js
@@ -0,0 +1,62 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const mocha = require('mocha');
+const describe = mocha.describe;
+
+const httpCode = require('../../app/resources/httpCodes');
+const middleware = require('../../app/middleware/middleware');
+
+const url = 'http://localhost:8000';
+let adminToken, teacherToken, studentToken;
+
+chai.use(chaiHttp);
+
+describe('Testing Users', function () {
+    before(function () {
+        adminToken = 'Bearer ' + middleware.generateToken('superuser@academy.com', 'admin');
+        teacherToken = 'Bearer ' + middleware.generateToken('teacher@academy.com', 'teacher');
+        studentToken = 'Bearer ' + middleware.generateToken('student@academy.com', 'student');
+
+    });
+
+    it('should return a no token provided code', function (done) {
+        chai.request(url)
+            .get("/users")
+            .end(function (err, res) {
+                expect(res).to.have.status(httpCode.codes.NOCONTENT);
+                done();
+            })
+    });
+
+    it('should return an array of users', function (done) {
+        chai.request(url)
+            .get("/users")
+            .set('Authorization', adminToken)
+            .end(function (err, res) {
+                expect(res).to.have.status(httpCode.codes.OK);
+                expect(res.body).to.be.a('array');
+                done();
+            })
+    });
+
+    it('should return an UNAUTHORIZED code', function (done) {
+        chai.request(url)
+            .get("/users")
+            .set('Authorization', teacherToken)
+            .end(function (err, res) {
+                expect(res).to.have.status(httpCode.codes.UNAUTHORIZED);
+                done();
+            })
+    });
+
+    it('should return an UNAUTHORIZED code', function (done) {
+        chai.request(url)
+            .get("/users")
+            .set('Authorization', studentToken)
+            .end(function (err, res) {
+                expect(res).to.have.status(httpCode.codes.UNAUTHORIZED);
+                done();
+            })
+    });
+})
