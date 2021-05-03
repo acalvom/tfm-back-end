d3f7bc41038d62105a6115ad01bcc7d2d2115f9a
acalvom <andreac.3108@gmail.com>
2021-05-03 17:59:52 +0200

    implement editUser route & controller #11

--- app/routes/usersRoutes.js
+++ app/routes/usersRoutes.js
@@ -9,5 +9,6 @@ router.use(helmet.hidePoweredBy());
 router.get('/users', middleware.isAdmin, usersController.getAllUsers);
 router.get('/users/students', middleware.isTeacher, usersController.getAllStudents);
 router.delete('/users/:email', middleware.isAdmin, usersController.deleteUser);
+router.put('/users/:email', middleware.isAdmin, usersController.editUser);
 
 module.exports = router;
