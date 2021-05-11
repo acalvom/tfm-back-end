5fb4471d9d688072613a74140fad2d73f35ff7ac
acalvom <andreac.3108@gmail.com>
2021-05-11 18:47:07 +0200

    implement the query to insert a workout #14

--- app/controllers/workoutsController.js
+++ app/controllers/workoutsController.js
@@ -1,7 +1,24 @@
+const httpCode = require("../resources/httpCodes");
 const workoutsController = {};
+const connection = require('../database/database');
+
+let sql;
 
 workoutsController.createWorkout = (req, res) => {
-    console.log(req);
+    console.log(req.body);
+    let workout = req.body;
+    if (Object.keys(workout).length === 0)
+        res.status(httpCode.codes.NOCONTENT).json('No workout sent');
+    else {
+        sql = 'INSERT INTO workouts SET ?';
+        connection.query(sql, [workout], function (err, resultDB) {
+            console.log(resultDB.insertId)
+            if (!err)
+                res.status(httpCode.codes.CREATED).json(resultDB.insertId + ' ' + workout);
+            else
+                res.status(httpCode.codes.CONFLICT).json('Workout already exists');
+        });
+    }
 }
 
 module.exports = workoutsController;
