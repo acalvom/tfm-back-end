3735ad97d57f0d7bbc50df769a2358b5c0a73ac3
acalvom <andreac.3108@gmail.com>
2021-05-16 21:29:21 +0200

    add test 'Create Class' & coverage report #22

--- app/controllers/classesController.js
+++ app/controllers/classesController.js
@@ -10,13 +10,13 @@ classesController.createClass = (req, res) => {
         res.status(httpCode.codes.NOCONTENT).json('No class sent');
     else {
         sql = 'INSERT INTO classes SET ?';
-        console.log(newClass)
+        // console.log(newClass)
         connection.query(sql, [newClass], function (err, resultDB) {
             if (!err) {
                 newClass.id = resultDB.insertId;
                 res.status(httpCode.codes.CREATED).json(newClass);
             } else
-                res.status(httpCode.codes.SERVERERROR).json('Internal Server Error');
+                res.status(httpCode.codes.CONFLICT).json('Class ' + newClass.code + ' already exists');
         });
     }
 }
